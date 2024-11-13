dd68246307586317b436ae3c0d841e47c6956f3f
Valeriehavey-ga <valeriehavey@gmail.com>
2024-11-12 21:13:19 -0800

    complete

--- app.js
+++ app.js
@@ -106,23 +106,92 @@ const userProfile = {
 
 console.log('Exercise 6 results:', userProfile);
 
+//Exercise 7:
+console.log('Exercise 7 results: see exportingFile.js and importingFile.js');
 
 
 
-//Exercise 7: 
 
-// export const myNumber = 123;
-// export const myString = 'Hello';
+//Exercise 8:
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
 
-// export default function superCoolFunction() {
-//     /* ... */
-//   }
-  
-//   import { myNumber, myString } from './myData.js';
+// 1. `cat`
 
-//   import superCoolFunction from './superCoolFunction.js';
+// 2. `white`
 
-//   import * as MyData from './myData.js';
-//   console.log(MyData.myNumber);
-//   console.log(MyData.myString);
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
 
+// Your code here
+
+
+function makeASentence(noun = "cat", adjective = "white") {
+  console.log(`Exercise 8 result: The ${noun} is ${adjective}.`);
+};
+
+makeASentence();
+
+
+
+//Exercise 9:
+// Convert the following `if...else` statement in to a ternary:
+
+// let pizza = 'tasty';
+
+// if (pizza === 'tasty') {
+//   console.log('yum');
+// } else {
+//   console.log('yuck');
+// }
+
+// Your code here
+
+const pizza = 'tasty';
+pizza === 'tasty' ? console.log('Exercise 9:', 'yum') : console.log('yuck');
+
+
+//Exercise 10(1):
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+
+const LANG = localLangConfig || 'en';
+
+// Log the result
+console.log('Exercise 10(1) result:', 'Language setting:', LANG);
+
+
+//Exercise 10(2):
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+
+const USER_THEME = userSavedTheme || 'Light';
+
+// Log the result
+console.log('Exercise 10(2) result:', 'User theme setting:', USER_THEME);
+
+
+//Exercise 11:
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+  name: 'Alice',
+};
+
+let cat = adventurer.cat?.age;
+
+console.log('Exercise 11 result:', cat);
